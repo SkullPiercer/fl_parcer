4c8abd11fc62174cb1a55a3294d40afa1f40d5cf
Александр Денисов <akatsuki_sanya@vk.com>
2024-06-29 13:00:30 +0500

    HH done

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 import telebot
 
-from utils import generate_keyboard
+from utils import generate_keyboard, get_data_from_hh
+
 
 load_dotenv()
 
@@ -19,6 +20,23 @@ def start(message):
         chat_id,
         'Выберите платформу: ',
         reply_markup=generate_keyboard()
+    )
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('parse_'))
+def send_posts(call):
+    """Sends vacancies with links."""
+    sites = {
+        'fl.ru': ...,
+        'habr-freelance': ...,
+        'hh.ru': get_data_from_hh()
+    }
+    chat_id = call.message.chat.id
+    posts = sites[call.data.split('_')[1]]
+    bot.send_message(
+        chat_id,
+        posts,
+        parse_mode='Markdown'
     )
 
 
--- utils.py
+++ utils.py
@@ -5,25 +5,36 @@ from dotenv import load_dotenv
 from telebot import types
 from bs4 import BeautifulSoup
 
-#
+
+# Service variables
 load_dotenv()
 RESOURCES = {
     'fl.ru': '',
     'habr-freelance': '',
-    'hh.ru': ''
+    'hh.ru': os.getenv('HH')
 }
 HEADERS = {'User-Agent': os.getenv('USER_AGENT')}
-RESPONSE_URL = os.getenv('URL')
 
 
-#
-def get_data_from_site(site_title):
-    response = requests.get(RESPONSE_URL, headers=HEADERS)
-    return response.status_code
-
-
-print(get_data_from_site('fl.ru'))
-print(HEADERS)
+# Parse functions
+def get_data_from_hh():
+    """Parsing first page of HH with Python filter."""
+    response = requests.get(RESOURCES['hh.ru'], headers=HEADERS)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    element_class = 'vacancy-card--z_UXteNo7bRGzxWVcL7y font-inter'
+    title_class = 'vacancy-name--c1Lay3KouCl7XasYakLk serp-item__title-link'
+    link_class = 'bloko-link'
+    posts = soup.find_all('div', class_=element_class)
+    posts_title = [
+        title.find('span', class_=title_class).get_text() for title in posts
+    ]
+    posts_link = [
+        link.find('a', class_=link_class).get('href') for link in posts
+    ]
+    result_message = ''
+    for title, link in zip(posts_title, posts_link):
+        result_message += f'[{title}]({link})\n'
+    return result_message
 
 
 # Keyboards
